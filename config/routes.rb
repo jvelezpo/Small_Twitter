aaed99d12c03be23f7dd0f6dddf54a5ba768d31a
Sebastian <sebasvelez@gmail.com>
2011-11-26 11:39:47 -0500

    Add Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 App::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,7 @@ App::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "It should have the rigth title" do
+      get 'home'
+      response.should have_selector("title", :content => "Ruby on Rails | Home")
     end
   end
 
@@ -15,5 +20,10 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
