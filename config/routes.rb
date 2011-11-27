43ba4a38af813de253b5c4bb9290bc772b52367e
Sebastian <sebasvelez@gmail.com>
2011-11-27 12:16:49 -0500

    New controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 App::Application.routes.draw do
+  root :to => "pages#home"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   
-  get "pages/home"
- 
-
+  match '/singup', :to => 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,7 +56,7 @@ App::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pages#home"
+  
 
   # See how all your routes lay out with "rake routes"
 
