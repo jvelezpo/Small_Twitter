c6990edaa6b22fb49c7296e5fdcdece819b7455c
Sebastian <sebasvelez@gmail.com>
2011-11-27 22:53:11 -0500

    authenticate method ready!

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,34 @@ class User < ActiveRecord::Base
 						 
 	before_save :encrypt_password
 	
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	
+	def User.authenticate(email, submitted_password)
+		user = User.find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+	
 	private
 	
 	def encrypt_password
+		self.salt = make_salt if new_record?
 		self.encrypted_password = encrypt(self.password)
 	end
 				 
 	def encrypt(string)
-		string
-	end			 
-					 				  
+		secure_hash("#{salt}--#{string}")
+	end		
 	
+	def make_salt
+		secure_hash("#{Time.now.utc}--#{password}")
+	end	 
+					 				  
+	def secure_hash(string)
+		Digest::SHA2.hexdigest(string)
+	end
 end
 
 
