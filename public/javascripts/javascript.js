ffacf0f729f152849129ce9bd55c3c0fed769195
Sebastian <sebasvelez@gmail.com>
2011-11-29 20:01:58 -0500

    JavaScript

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,9 +1,5 @@
-function ruben(){
-	alert("El valor: " + fomularioRuben.ruben.options[fomularioRuben.ruben.selectedIndex].value);
-	alert("El id: " + fomularioRuben.ruben.options[fomularioRuben.ruben.selectedIndex].id);
+function prueba(){
+	alert("El valor: " + fomularioJ.pruebita.options[fomularioJ.pruebita.selectedIndex].value);
+	alert("El id: " + fomularioJ.pruebita.options[fomularioJ.pruebita.selectedIndex].id);
 }
 
-function mateo(){
-	alert("El valor: " + fomularioMateo.mateo.options[fomularioMateo.mateo.selectedIndex].value);
-	alert("El id: " + fomularioMateo.mateo.options[fomularioMateo.mateo.selectedIndex].id);
-}
