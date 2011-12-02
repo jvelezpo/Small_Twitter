b94bc6f22c9efee2702f1bb3389389a46fe7fa9d
Sebastian <sebasvelez@gmail.com>
2011-12-02 06:39:18 -0500

    Sing up for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,15 @@ class UsersController < ApplicationController
   	@title = @user.name
   end
   	
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Wellcome to the best WebApp ever! " + @user.name
+  		redirect_to user_path(@user)
+  	else
+  		@title = "Sing up"
+  		render 'new'
+  	end	
+  end
 
 end
