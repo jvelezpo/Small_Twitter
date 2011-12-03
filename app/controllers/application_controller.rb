23ecd115516f8a044aef72fa015da5efb9a9e64a
Sebastian <sebasvelez@gmail.com>
2011-12-03 10:46:29 -0500

    sing in working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include sessions_helper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,81 @@ class SessionsController < ApplicationController
   end
   
   def create
-  	
+  	user = User.authenticate(params[:session][:email], 
+  							 params[:session][:password])
+  	if user.nil?
+  		flash.now[:error] = "Nada mono! email/password combination"
+  		@title = "Sing in"
+  		render 'new'
+  	else		
+  		sing_in user
+  		redirect_to user_path(user)
+  	end	
   end
   	
   def destroy
   	
   end
 
+  
+
+
+
+
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
