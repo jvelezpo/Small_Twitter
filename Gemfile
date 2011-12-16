1ff9185b7f7570a73843e10f363ac727ae1fdf5f
Sebastian <sebasvelez@gmail.com>
2011-12-16 07:41:30 -0500

    crud de users completed!

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
 gem 'gravatar_image_tag'
-gem 'will_paginate', '3.0.pre2'
+
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
 	
   def index
-  	@users = User.paginate(:page => params[:page])
+  	@users = User.all
   	@title = "All users"	
   end
   
@@ -45,6 +46,12 @@ class UsersController < ApplicationController
   	end
   end
   
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "User destroyed."
+  	redirect_to users_path
+  end
+  
   private
   
   def authenticate
@@ -55,6 +62,12 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	redirect_to(current_user) unless current_user?(@user)
   end  
+  
+  def admin_user
+  	user = User.find(params[:id])
+  	flash[:error] = "No te puedes borrar a ti mismo"
+  	redirect_to(users_path) if (!current_user.admin? || current_user?(user))
+  end
 end
 
 
